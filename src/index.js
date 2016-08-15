ae53b63c7cd5be0522ad09ad5e26ebdf84c44f7d
Todd Kirk <toddjkirk@gmail.com>
2016-08-15 11:59:48 -0500

    added redux trouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {Router, Route, browserHistory} from 'react-router';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -9,7 +10,9 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <Router history={browserHistory}>
+                <Route path="/" component={App}></Route>
+        </Router>
+    </Provider>
+    , document.querySelector('.container'));
